31825137370bf2764a2ab56fb7022334a7ee8521
clenberger <christian.lenberger@gmail.com>
2019-10-17 13:48:34 -0700

    added MongoDB and worked on DB Structure during this sprint

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, redirect, url_for
 
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.StreetwearDB
+clothing = db.clothing
 
 app = Flask(__name__)
 
+# clothing.insert_many([
+#     {'name': 'Flowers T-Shirt', 'brand': 'Supreme'},
+#     {'name': 'ASSC Logo Hoodie', 'brand': 'ASSC'},
+#     {'name': 'BOGO Hoodie', 'brand': 'Supreme'}
+# ])
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return 'Hello, welcome to SWDB!'
+def swdb_index():
+    """Show clothing feed."""
+    clothing_index = clothing.find()
+    return render_template('clothing_index.html', clothing_index=clothing_index)
 
 if __name__ == '__main__':
     app.run(debug=True)
